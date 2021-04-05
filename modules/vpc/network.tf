707e38f3dea76ead2ab19d0ba47e4127820ba4c9
Royston Pierre-Pinnock <roysto.pierre_pinnoc@iris.co.uk>
2021-04-05 18:14:12 +0100

    modules

---
+++ modules/vpc/network.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    environment = "dev"
+    name        = var.name
+  }
+}
+
+
+
---
+++ modules/vpc/vars.tf
@@ -0,0 +1,8 @@
+variable "vpc_cidr_block" {}
+variable "name" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
+variable "enable_nat_gateway" {}
+variable "single_nat_gateway" {}
+variable "enable_dns_hostnames" {}
+variable "public_subnet_tags" {}
