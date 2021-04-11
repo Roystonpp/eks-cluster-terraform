059b0acea8800f6a4c5fb6e070141d68d70d25b3
Royston Pierre-Pinnock <roysto.pierre_pinnoc@iris.co.uk>
2021-04-11 20:14:15 +0100

    eks resource with policy

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -1,11 +1,31 @@
 resource "aws_eks_cluster" "dev_cluster" {
-  name = var.cluster_name
-  role_arn = ""
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
   vpc_config {
-    subnet_ids = []
+    subnet_ids = "${var.subnets}"
   }
 }
 
-output "" {
-  value = ""
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = <<POLICY
+{
+"Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
 }
