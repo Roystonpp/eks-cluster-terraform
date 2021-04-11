b235c025eeecec26a9662fc9dc355e7419b21eb0
Royston Pierre-Pinnock <roysto.pierre_pinnoc@iris.co.uk>
2021-04-11 18:28:45 +0100

    eks module

--- dev/main.tf
+++ dev/main.tf
@@ -32,6 +32,9 @@ module "vpc" {
 module "eks" {
   source = "../modules/eks"
 
-  vpc_id = module.vpc.vpc_id
+  cluster_name    = local.cluster_name
+  cluster_version = "1.17"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
 }
