91fa1efb873bb8dea127ab6b4eaab3478a25edb1
Royston Pierre-Pinnock <roysto.pierre_pinnoc@iris.co.uk>
2021-05-10 10:17:27 +0100

    eks vars & outputs

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -4,9 +4,20 @@ resource "aws_eks_cluster" "dev_cluster" {
 
   vpc_config {
     subnet_ids = "${var.subnets}"
+
+    # Indicates if Amazon EKS private API server endpoint is enabled (good for bastion or vpn connection)
+    endpoint_private_access = var.endpoint_private_access
+    # Indicates if Amazon EKS public API server endpoint is enabled
+    endpoint_public_access  = var.endpoint_public_access
   }
+
+  # Ensures permissions are created before EKS cluster creation
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-cluster-attachment
+  ]
 }
 
+# The role that Amazon EKS will use to create AWS resources for the Kubernetes cluster
 resource "aws_iam_role" "eks_cluster_role" {
   name               = "eks-cluster-role"
   assume_role_policy = <<POLICY
@@ -27,5 +38,7 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "eks-cluster-attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+
+  # The role the policy should be applied to
   role       = aws_iam_role.eks_cluster_role.name
 }
--- modules/eks/vars.tf
+++ modules/eks/vars.tf
@@ -2,3 +2,5 @@ variable "cluster_name" {}
 variable "cluster_version" {}
 variable "subnets" {}
 variable "vpc_id" {}
+variable "endpoint_private_access" {}
+variable "endpoint_public_access" {}
